7c18c52812a806ac054922a0276068a7e4494bec
ethanchuang2004 <ethan.chuang.2004@gmail.com>
2020-09-26 18:59:07 -0700

    dominant color finding
    found some online code to help and trying to understand more complex cv functions.

--- project/loading.py
+++ project/loading.py
@@ -63,10 +63,38 @@ hist = cv2.calcHist([image], [0, 1, 2],
 cv2.destroyAllWindows()
 plt.show()
 
+# code to help find dominant color of image found at
+# https://www.timpoulsen.com/2018/finding-the-dominant-colors-of-an-image.html
+img = image
+height, width, _ = np.shape(img)
+
+# calculate the average color of each row of our image
+avg_color_per_row = np.average(img, axis=0)
+
+# calculate the averages of our rows
+avg_colors = np.average(avg_color_per_row, axis=0)
+
+# avg_color is a tuple in BGR order of the average colors
+# but as float values
+print(f'avg_colors: {avg_colors}')
+
+# so, convert that array to integers
+int_averages = np.array(avg_colors, dtype=np.uint8)
+print(f'int_averages: {int_averages}')
+
+# create a new image of the same height/width as the original
+average_image = np.zeros((height, width, 3), np.uint8)
+# and fill its pixels with our average color
+average_image[:] = int_averages
+
+# finally, show it side-by-side with the original
+cv2.imshow("Avg Color", average_image)
+
 
 # Chapter 9 Threshholding / Chapter 8 Blurring
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
